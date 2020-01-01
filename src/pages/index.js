85e9f0425b17473f075a0b80f014beb6a92e82ef
Marcel Vogel <marcel.vogel@gmx.ch>
2020-01-01 22:25:49 +0100

    feat: change of colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
-
+export default () => (
+<div style={{ color: `purple` }}></div>
 export default () => <div>Hello Gatsby - Hallo Weltretter</div>
+<p>What a world.</p>
+</div>
+)
